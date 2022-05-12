8e4cb676854abce81a01d2012556837f4a90a300
Your Name <you@example.com>
2022-05-13 00:57:29 +0100

    commit

--- Project - Exploring Near-Earth Objects/filters.py
+++ Project - Exploring Near-Earth Objects/filters.py
@@ -1,19 +1,15 @@
-"""Provide filters for querying close approaches and limit the generated results.
-
+"""Provide filters for querying close approaches and limiting results.
 The `create_filters` function produces a collection of objects that is used by
 the `query` method to generate a stream of `CloseApproach` objects that match
 all of the desired criteria. The arguments to `create_filters` are provided by
 the main module and originate from the user's command-line options.
-
 This function can be thought to return a collection of instances of subclasses
 of `AttributeFilter` - a 1-argument callable (on a `CloseApproach`) constructed
 from a comparator (from the `operator` module), a reference value, and a class
-method `get` that subclasses can override to fetch an attribute of interest from
-the supplied `CloseApproach`.
-
+method `get` that subclasses can override to fetch an attribute of interest
+from the supplied `CloseApproach`.
 The `limit` function simply limits the maximum number of values produced by an
 iterator.
-
 You'll edit this file in Tasks 3a and 3c.
 """
 import operator
@@ -25,27 +21,23 @@ class UnsupportedCriterionError(NotImplementedError):
 
 class AttributeFilter:
     """A general superclass for filters on comparable attributes.
-
     An `AttributeFilter` represents the search criteria pattern comparing some
-    attribute of a close approach (or its attached NEO) to a reference value. It
-    essentially functions as a callable predicate for whether a `CloseApproach`
-    object satisfies the encoded criterion.
-
+    attribute of a close approach (or its attached NEO) to a reference value.
+    It essentially functions as a callable predicate for whether a
+    `CloseApproach` object satisfies the encoded criterion.
     It is constructed with a comparator operator and a reference value, and
     calling the filter (with __call__) executes `get(approach) OP value` (in
     infix notation).
-
     Concrete subclasses can override the `get` classmethod to provide custom
     behavior to fetch a desired attribute from the given `CloseApproach`.
     """
-    def __init__(self, op, value):
-        """Construct a new `AttributeFilter` from an binary predicate and a reference value.
 
+    def __init__(self, op, value):
+        """Construct a new `AttributeFilter` from a binary predicate and value.
         The reference value will be supplied as the second (right-hand side)
         argument to the operator function. For example, an `AttributeFilter`
         with `op=operator.le` and `value=10` will, when called on an approach,
         evaluate `some_attribute <= 10`.
-
         :param op: A 2-argument predicate comparator (such as `operator.le`).
         :param value: The reference value to compare against.
         """
@@ -59,92 +51,19 @@ class AttributeFilter:
     @classmethod
     def get(cls, approach):
         """Get an attribute of interest from a close approach.
-
         Concrete subclasses must override this method to get an attribute of
         interest from the supplied `CloseApproach`.
-
         :param approach: A `CloseApproach` on which to evaluate this filter.
-        :return: The value of an attribute of interest, comparable to `self.value` via `self.op`.
+        :return: The value of an attribute of interest, comparable to
+            `self.value` via `self.op`.
         """
         raise UnsupportedCriterionError
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(op=operator.{self.op.__name__}, value={self.value})"
+        """Return `repr(self)` string representation of object."""
+        return f"{self.__class__.__name__}(op=operator.{self.op.__name__}, \
+            value={self.value})"
 
-class DateFilter(AttributeFilter):
-    """Subclass of AttributeFilter to filter CloseApproach objects by date."""
-
-    @classmethod
-    def get(cls, approach):
-        """Return approach.time converted to datetime.datetime object for the date filter.
-        
-        Args:
-            approach (CloseApproach): A CloseApproach object.
-        Returns:
-            [datetime.datetime]: Converted time to datetime object.
-            
-        """
-        return approach.time.date()
-
-class DistanceFilter(AttributeFilter):
-    """Subclass of AttributeFilter to filter approach objects by distance."""
-
-    @classmethod
-    def get(cls, approach):
-        """Return distance of the CloseApproach objectfor the distance filter.
-        
-        Args:
-            approach (CloseApproach): A CloseApproach object.
-        Returns:
-            [float]: Returns the distance of a CloseApproach.
-            
-        """
-        return approach.distance
-    
-class VelocityFilter(AttributeFilter):
-    """Subclass of AttributeFilter to filter approach objects by velocity."""
-
-    @classmethod
-    def get(cls, approach):
-        """Return approach.velocity for the velocity filter.
-        
-        Args:
-            approach (CloseApproach): A CloseApproach object.
-        Returns:
-            [float]: Returns the velocity of a CloseApproach.
-            
-        """
-        return approach.velocity
-    
-class DiameterFilter(AttributeFilter):
-    """Subclass of AttributeFilter to filter approach objects by diameter."""
-
-    @classmethod
-    def get(cls, approach):
-        """Return the diameter of the neo assigned to the CloseApproach object for the diameter filter.
-        
-        Args:
-            approach (CloseApproach): A CloseApproach object.
-        Returns:
-            [float]: Returns the diameter of a NearEarthObject object.
-            
-        """
-        return approach.neo.diameter
-
-class HazardousFilter(AttributeFilter):
-    """Subclass to filter CloseApproach objects by if it's hazardous."""
-
-    @classmethod
-    def get(cls, approach):
-        """Return the hazardous attribute of the neo assigned to the CloseApproach object for the diameter filter.
-        
-        Args:
-            approach (CloseApproach): A CloseApproach object.
-        Returns:
-            [float]: Returns the hazardous attribute of a NearEarthObject object.
-            
-        """
-        return approach.neo.hazardous
 
 def create_filters(
         date=None, start_date=None, end_date=None,
@@ -154,69 +73,142 @@ def create_filters(
         hazardous=None
 ):
     """Create a collection of filters from user-specified criteria.
-
-    Each of these arguments is provided by the main module with a value from the
-    user's options at the command line. Each one corresponds to a different type
-    of filter. For example, the `--date` option corresponds to the `date`
-    argument, and represents a filter that selects close approaches that occurred
-    on exactly that given date. Similarly, the `--min-distance` option
+    Each of these arguments is provided by the main module with a value from
+    the user's options at the command line. Each one corresponds to a different
+    type of filter. For example, the `--date` option corresponds to the `date`
+    argument, and represents a filter that selects close approaches that
+    occurred on exactly that given date. Similarly, the `--min-distance` option
     corresponds to the `distance_min` argument, and represents a filter that
     selects close approaches whose nominal approach distance is at least that
     far away from Earth. Each option is `None` if not specified at the command
     line (in particular, this means that the `--not-hazardous` flag results in
     `hazardous=False`, not to be confused with `hazardous=None`).
-
-    The return value must be compatible with the `query` method of `NEODatabase`
-    because the main module directly passes this result to that method. For now,
-    this can be thought of as a collection of `AttributeFilter`s.
-
+    The return value must be compatible with the `query` method of
+    `NEODatabase` because the main module directly passes this result to that
+    method. For now, this can be thought of as a collection of
+    `AttributeFilter`s.
     :param date: A `date` on which a matching `CloseApproach` occurs.
-    :param start_date: A `date` on or after which a matching `CloseApproach` occurs.
-    :param end_date: A `date` on or before which a matching `CloseApproach` occurs.
-    :param distance_min: A minimum nominal approach distance for a matching `CloseApproach`.
-    :param distance_max: A maximum nominal approach distance for a matching `CloseApproach`.
-    :param velocity_min: A minimum relative approach velocity for a matching `CloseApproach`.
-    :param velocity_max: A maximum relative approach velocity for a matching `CloseApproach`.
-    :param diameter_min: A minimum diameter of the NEO of a matching `CloseApproach`.
-    :param diameter_max: A maximum diameter of the NEO of a matching `CloseApproach`.
-    :param hazardous: Whether the NEO of a matching `CloseApproach` is potentially hazardous.
+    :param start_date: A `date` on or after which a matching `CloseApproach`
+        occurs.
+    :param end_date: A `date` on or before which a matching `CloseApproach`
+        occurs.
+    :param distance_min: A minimum nominal approach distance for a matching
+        `CloseApproach`.
+    :param distance_max: A maximum nominal approach distance for a matching
+        `CloseApproach`.
+    :param velocity_min: A minimum relative approach velocity for a matching
+        `CloseApproach`.
+    :param velocity_max: A maximum relative approach velocity for a matching
+        `CloseApproach`.
+    :param diameter_min: A minimum diameter of the NEO of a matching
+        `CloseApproach`.
+    :param diameter_max: A maximum diameter of the NEO of a matching
+        `CloseApproach`.
+    :param hazardous: Whether the NEO of a matching `CloseApproach` is
+        potentially hazardous.
     :return: A collection of filters for use with `query`.
     """
     filters = []
-    
-    if date:
-        filters.append(DateFilter(operator.eq, date))
-    if start_date:
-        filters.append(DateFilter(operator.ge, start_date))
-    if end_date:
-        filters.append(DateFilter(operator.le, end_date))
-    if distance_min:
-        filters.append(DistanceFilter(operator.ge, distance_min))
-    if distance_max:
-        filters.append(DistanceFilter(operator.le, distance_max))
-    if velocity_min:
-        filters.append(VelocityFilter(operator.ge, velocity_min))
-    if velocity_max:
-        filters.append(VelocityFilter(operator.le, velocity_max))
-    if diameter_min:
-        filters.append(DiameterFilter(operator.ge, diameter_min))
-    if diameter_max:
-        filters.append(DiameterFilter(operator.le, diameter_max))
+
+    if date is not None:
+        filters.append(
+            CreateFilter(operator.eq, date, 'approach.time', fn='date')
+        )
+
+    if start_date is not None:
+        filters.append(
+            CreateFilter(operator.ge, start_date, 'approach.time', fn='date')
+        )
+
+    if end_date is not None:
+        filters.append(
+            CreateFilter(operator.le, end_date, 'approach.time', fn='date')
+        )
+
+    if distance_min is not None:
+        filters.append(
+            CreateFilter(operator.ge, distance_min, 'approach.distance')
+        )
+
+    if distance_max is not None:
+        filters.append(
+            CreateFilter(operator.le, distance_max, 'approach.distance')
+        )
+
+    if velocity_min is not None:
+        filters.append(
+            CreateFilter(operator.ge, velocity_min, 'approach.velocity')
+        )
+
+    if velocity_max is not None:
+        filters.append(
+            CreateFilter(operator.le, velocity_max, 'approach.velocity')
+        )
+
+    if diameter_min is not None:
+        filters.append(
+            CreateFilter(operator.ge, diameter_min, 'approach.neo.diameter')
+        )
+
+    if diameter_max is not None:
+        filters.append(
+            CreateFilter(operator.le, diameter_max, 'approach.neo.diameter')
+        )
+
     if hazardous is not None:
-        filters.append(HazardousFilter(operator.eq, hazardous))
+        filters.append(
+            CreateFilter(operator.eq, hazardous, 'approach.neo.hazardous')
+        )
 
     return filters
 
 
 def limit(iterator, n=None):
     """Produce a limited stream of values from an iterator.
-
     If `n` is 0 or None, don't limit the iterator at all.
-
     :param iterator: An iterator of values.
     :param n: The maximum number of values to produce.
     :yield: The first (at most) `n` values from the iterator.
     """
-    if n == 0 or n is None:
-        return iterator
-    return [x for i, x in enumerate(iterator) if i<n]
+    if n is None or n == 0:
+        for i, element in enumerate(iterator):
+            try:
+                yield element
+            except StopIteration:
+                pass
+    else:
+        it = iter(range(n))
+        for i, element in enumerate(iterator):
+            try:
+                nexti = next(it)
+                yield element
+            except StopIteration:
+                pass
+
+
+def CreateFilter(op, value, param, fn=None):
+    """Create a filter class object based on the passed in parameters."""
+
+    class GeneralFilter(AttributeFilter):
+        def __init__(self):
+            self.op = op
+            self.value = value
+
+        def __call__(self, approach):
+            return self.op(self.get(approach), self.value)
+
+        @classmethod
+        def get(cls, approach):
+            params = param.split('.')
+
+            v = locals()[params[0]]
+
+            for elem in params[1:]:
+                v = getattr(v, elem)
+
+            if fn:
+                return getattr(v, fn)()
+            else:
+                return v
+
+    return GeneralFilter()
--- Project - Exploring Near-Earth Objects/write.py
+++ Project - Exploring Near-Earth Objects/write.py
@@ -1,51 +1,100 @@
 """Write a stream of close approaches to CSV or to JSON.
-
 This module exports two functions: `write_to_csv` and `write_to_json`, each of
 which accept an `results` stream of close approaches and a path to which to
 write the data.
-
 These functions are invoked by the main module with the output of the `limit`
 function and the filename supplied by the user at the command line. The file's
 extension determines which of these functions is used.
-
 You'll edit this file in Part 4.
 """
 import csv
 import json
+import helpers
 
 
 def write_to_csv(results, filename):
     """Write an iterable of `CloseApproach` objects to a CSV file.
-
-    The precise output specification is in `README.md`. Roughly, each output row
-    corresponds to the information in a single close approach from the `results`
-    stream and its associated near-Earth object.
-
+    The precise output specification is in `README.md`. Roughly, each output
+    row corresponds to the information in a single close approach from the
+    `results` stream and its associated near-Earth object.
     :param results: An iterable of `CloseApproach` objects.
-    :param filename: A Path-like object pointing to where the data should be saved.
+    :param filename: A Path-like object pointing to where the data should be
+        saved.
     """
     fieldnames = (
         'datetime_utc', 'distance_au', 'velocity_km_s',
         'designation', 'name', 'diameter_km', 'potentially_hazardous'
     )
-    with open(filename, 'w') as f:
-        writer = csv.DictWriter(f, fieldnames)
-        writer.writeheader()
-        for elem in results:
-            writer.writerow(elem.to_csv())
+
+    with open(filename, 'w') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(fieldnames)
+
+        for row in results:
+            result_dict = {field: None for field in fieldnames}
+
+            result_dict['datetime_utc'] = helpers.datetime_to_str(row.time)
+            result_dict['distance_au'] = row.distance
+            result_dict['velocity_km_s'] = row.velocity
+            result_dict['designation'] = row.neo.designation
+
+            if result_dict['name'] is None:
+                result_dict['name'] = ''
+
+            if row.neo.diameter == float('nan'):
+                result_dict['diameter_km'] = 'nan'
+            else:
+                result_dict['diameter_km'] = row.neo.diameter
+
+            result_dict['potentially_hazardous'] = row.neo.hazardous
+
+            writer.writerow(result_dict.values())
 
 
 def write_to_json(results, filename):
     """Write an iterable of `CloseApproach` objects to a JSON file.
-
-    The precise output specification is in `README.md`. Roughly, the output is a
-    list containing dictionaries, each mapping `CloseApproach` attributes to
+    The precise output specification is in `README.md`. Roughly, the output is
+    a list containing dictionaries, each mapping `CloseApproach` attributes to
     their values and the 'neo' key mapping to a dictionary of the associated
     NEO's attributes.
-
     :param results: An iterable of `CloseApproach` objects.
-    :param filename: A Path-like object pointing to where the data should be saved.
+    :param filename: A Path-like object pointing to where the data should be
+        saved.
     """
-    json_results = [elem.to_json() for elem in results]
-    with open(filename, 'w') as f:
-        json.dump(json_results, f, indent=2)
+    fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'neo')
+
+    data = []
+
+    for row in results:
+        result_dict = {field: None for field in fieldnames}
+
+        result_dict['datetime_utc'] = helpers.datetime_to_str(row.time)
+        result_dict['distance_au'] = row.distance
+        result_dict['velocity_km_s'] = row.velocity
+
+        neo_dict = {
+            'designation': None, 'name': None,
+            'diameter_km': None, 'potentially_hazardous': None
+        }
+
+        neo_dict['designation'] = row.neo.designation
+
+        if row.neo.name is None:
+            neo_dict['name'] = ''
+        else:
+            neo_dict['name'] = row.neo.name
+
+        if row.neo.diameter == float('nan'):
+            neo_dict['diameter_km'] = float('nan')
+        else:
+            neo_dict['diameter_km'] = row.neo.diameter
+
+        neo_dict['potentially_hazardous'] = row.neo.hazardous
+
+        result_dict['neo'] = neo_dict
+
+        data.append(result_dict)
+
+    with open(filename, 'w') as outfile:
+        json.dump(data, outfile)
+
