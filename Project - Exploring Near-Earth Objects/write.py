23cb1e29e83e8364c65860676fedb29751501618
Your Name <you@example.com>
2022-05-13 00:27:33 +0100

    commit

--- Project - Exploring Near-Earth Objects/write.py
+++ Project - Exploring Near-Earth Objects/write.py
@@ -28,14 +28,11 @@ def write_to_csv(results, filename):
         'datetime_utc', 'distance_au', 'velocity_km_s',
         'designation', 'name', 'diameter_km', 'potentially_hazardous'
     )
-    with open(filename, "w", newline="") as outfile:
-        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+    with open(filename, 'w') as f:
+        writer = csv.DictWriter(f, fieldnames)
         writer.writeheader()
-        for result in results:
-            content = {**result.serialize(), **result.neo.serialize()}
-            content["name"] = content["name"] if content["name"] is not None else ""
-            content["potentially_hazardous"] = "True" if content["potentially_hazardous"] else "False"
-            writer.writerow(content)
+        for elem in results:
+            writer.writerow(elem.to_csv())
 
 
 def write_to_json(results, filename):
@@ -49,24 +46,6 @@ def write_to_json(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    data = []
-    for result in results:
-        content = {**result.serialize(), **result.neo.serialize()}
-        content["name"] = content["name"] if content["name"] is not None else ""
-        content["potentially_hazardous"] = bool(1) if content["potentially_hazardous"] else bool(0)
-        data.append(
-            {
-                "datetime_utc": content["datetime_utc"],
-                "distance_au": content["distance_au"],
-                "velocity_km_s": content["velocity_km_s"],
-                "neo": {
-                    "designation": content["designation"],
-                    "name": content["name"],
-                    "diameter_km": content["diameter_km"],
-                    "potentially_hazardous": content["potentially_hazardous"],
-                },
-            }
-        )
-
-    with open(filename, "w") as outfile:
-        json.dump(data, outfile, indent="\t")
+    json_results = [elem.to_json() for elem in results]
+    with open(filename, 'w') as f:
+        json.dump(json_results, f, indent=2)
