3df9a688a6eb89cea718a508e17909d9294f155e
Your Name <you@example.com>
2022-06-04 10:21:09 +0100

    commit

--- Project 2 Submission/meme-generator-starter-code/QuoteEngine/IngesterInterface.py
+++ Project 2 Submission/meme-generator-starter-code/QuoteEngine/IngesterInterface.py
@@ -1,5 +1,17 @@
+'''
+IngesterInterface:
+The project contains an abstract base class, IngestorInterface, which defines:
+A complete classmethod method to verify if the file type is compatible with the ingestor class.
+An abstract method for parsing the file content
+(i.e., splitting each row) and outputting it to a Quote object.
+There is a class defined to support each supported file type:
+Text, Docx, Pdf,Csv
+'''
+
 from abc import ABC, abstractmethod
-# abc is a builtin module, we have to import ABC and abstractmethod
+from ast import In
+import pandas as pd
+
 
 class IngestorInterface(ABC): # Inherit from ABC(Abstract base class)
     @property
@@ -11,8 +23,43 @@ class IngestorInterface(ABC): # Inherit from ABC(Abstract base class)
     def parse(cls, path: str) -> List[QuoteModel]:
         pass
 
-    def can_ingest(self, path):
-        #extension = path.split('.')[-1]
-        #if extension == self.supported_file_type:
-        #    return True
-        #return False
+    def can_ingest(self, path) -> boolean:
+        pass
+
+class TextIngestor(IngestorInterface):
+    '''
+    The class inherits the IngestorInterface.
+    The class does not depend on any 3rd party library to complete the defined,
+    abstract method signatures to parse Text files.
+    The parse method returns a valid QuoteModel
+    '''
+    def file_types(self):
+
+class DocxIngestor(IngestorInterface):
+    '''
+    The class inherits from the IngestorInterface class.
+    The class depends on the python-docx library to complete the defined,
+    abstract method signatures to parse DOCX files.
+    The parse method returns a valid QuoteModel
+    '''
+
+class PDFIngestor(IngestorInterface):
+    '''
+    The project contains a PDFIngestor class.
+    The PDFIngestor class inherits from the IngestorInterface class.
+    The PDFIngestor class utilizes the subprocess module to call the pdftotext CLI utility,
+    creating a pipeline that converts PDFs to text and then ingests the text.
+    The class handles deleting temporary files.
+    The parse method returns a valid QuoteModel
+    '''
+
+class CSVIngestor(IngestorInterface):
+    '''
+    The class inherits the IngestorInterface.
+    The class depends on the pandas library to complete the defined, 
+    abstract method signatures to parse CSV files.
+    The parse method returns a valid QuoteModel
+    '''
+
+
+
--- Project 2 Submission/meme-generator-starter-code/QuoteEngine/QuoteModel.py
+++ Project 2 Submission/meme-generator-starter-code/QuoteEngine/QuoteModel.py
@@ -1,3 +1,9 @@
+'''
+A Python class that defines a QuoteModel object, 
+which contains text fields for body and author. 
+The class overrides the correct methods to instantiate the class,
+prints the model contents as: ”body text” - author
+'''
 
 class QuoteModel():
 
