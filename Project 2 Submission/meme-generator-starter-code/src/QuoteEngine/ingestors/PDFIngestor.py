a4aa9086f910b9c5078d8ece82bdbf9f8d99199b
Your Name <you@example.com>
2022-06-10 00:44:20 +0100

    commit

--- Project 2 Submission/meme-generator-starter-code/src/QuoteEngine/ingestors/PDFIngestor.py
+++ Project 2 Submission/meme-generator-starter-code/src/QuoteEngine/ingestors/PDFIngestor.py
@@ -1,13 +1,50 @@
-class PDFIngestor(IngestorInterface):
-    '''
-    The PDFIngestor class inherits from the IngestorInterface class.
-    The PDFIngestor class utilizes the subprocess module to call the pdftotext CLI utility,
-    creating a pipeline that converts PDFs to text and then ingests the text.
-    The class handles deleting temporary files.
-    The parse method returns a valid QuoteModel
-    '''
-    @property
-    def file_types(self):
-        '''Return Docx extension type'''
-        return "pdf"
+"""Define specific pdf ingestor."""
+import subprocess
+import os
+import random
+from typing import List
 
+from ..IngestorInterface import IngestorInterface
+from ..QuoteModel import QuoteModel
+
+
+class PDFIngestor(IngestorInterface):
+    """Realise the IngestorInterface abstract base class.
+    Implement specific parse method for .pdf files.
+    """
+
+    allowed_extensions = ['pdf']
+
+    @classmethod
+    def parse(cls, path: str) -> List[QuoteModel]:
+        """Parse the .pdf file to extract quotes.
+        Instantiate QuoteModel objects for each quote.
+        Return list of all QuoteModel Objects created from the file.
+        This method splits on a ' - '. However the method enables
+        the quotes themselves to contain the characters ' - '
+        :param path: the file path to be parsed.
+        """
+        try:
+            if not cls.can_ingest(path):
+                raise Exception('cannot ingest file')
+
+            quotes = []
+            tmp = f'./tmp/{random.randint(0,100000)}.txt'
+            call = subprocess.call(['pdftotext', '-raw', path, tmp])
+
+            with open(tmp, 'r', encoding='utf-8-sig') as f:
+                lines = f.readlines()
+                for line in lines:
+                    line = line.strip('\n\r')
+                    if len(line) > 0:
+                        parts = line.split(' - ')
+                        author = parts[-1]
+                        body_all = parts[0:len(parts)-1]
+                        body = ' '.join(body_all)
+                        new_quote = QuoteModel(body, author)
+                        quotes.append(new_quote)
+
+            os.remove(tmp)
+            return quotes
+        except Exception:
+            raise Exception('Error parsing file')
